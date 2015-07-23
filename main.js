1dc8d16ef6a898cdef66dfddd2b582b563195549
RyKilleen <deathbypapercut@gmail.com>
2015-07-23 16:15:48 -0400

    Major code refactor, silly old me.
    And a note to future me: Current me, right?

--- main.js
+++ main.js
@@ -1,99 +1,8 @@
-random = Math.round((Math.random() * 255));
-random2 = Math.round((Math.random() * 255));
-random3 = Math.round((Math.random() * 255));
-
-
-
-var box = $('<div id="box"></div');
-box.css("background-color", "rgb("+random2+","+random3+","+random+")");
-box.appendTo("body")
-
-
-
 $body = $("body");
 
-boxColor = getTheRGB(box.css("background-color"));
-backColor = getTheRGB($body.css("background-color"));
-
-var counter = 0;
-var countUp = function() {
-
-    	counter++;
-    	$("#timer .value").text(counter);
-    	return counter;
-    }
-
-var test= window.setInterval(countUp, 100);
-
-
-/*Touch? */
-
-// document.addEventListener('touchmove', function(e) {
-//     e.preventDefault();
-//     var touch = e.touches[0];
-
-//     enclosure(e);
-
-// }, false);
-
-var enclosure = function(e) { 
-
-	var $width = ($(document).width())/255;
-    var $height = ($(document).height())/255;
-    var $pageX = parseInt(e.pageX / $width,10);
-    var $pageY = parseInt(e.pageY / $height,10);
-    var difficulty = 5;
-    
-
-    var colorKeepUp = function(e) {
-
-		    
-	    $("body").css("background-color", "rgb("+$pageX+","+$pageY+","+random+")");
-	    backColor = getTheRGB($body.css("background-color"));
-	    boxColor = getTheRGB(box.css("background-color"));
-
-	}
-
-	var compareThem = function(a, b) {		
-
-		var rDiff = Math.abs(a[0] - b[0]);
-		var gDiff = Math.abs(a[1] - b[1]);
-		var bDiff = Math.abs(a[2] - b[2]);
-
-		// console.log(rDiff + "///" + gDiff + "///" + bDiff);
-
-
-		if (rDiff < difficulty && gDiff < difficulty && bDiff < difficulty) {
-			alert("You did it! And it only took you "+ ( counter / 10 ) + " seconds to do it!");
-	    	random = Math.round((Math.random() * 255));
-			random2 = Math.round((Math.random() * 255));
-			random3 = Math.round((Math.random() * 255));
-	    	$("body").css("background-color", "rgb("+$pageX+","+$pageY+","+random+")");
-	    	box.css("background-color", "rgb("+random2+","+random3+","+random+")");
-	    	counter = 0;
-	
-		}
-
-	}
-
-	colorKeepUp(e, false);
-	compareThem(backColor, boxColor);
-
-}
-
-
-
-
-
-$(document).mousemove(function(e){
-	
-	
-	enclosure(e);
-    
-
-}); 
-
-
+function randomColorValue () {
+	return Math.round((Math.random() * 255));
+};
 
 function getTheRGB(value) {
 	var a = value.split("(")[1].split(")")[0];
@@ -101,3 +10,111 @@ function getTheRGB(value) {
 	
 	return a;
 }
+
+var counter = {
+
+	val : 0,
+
+	$timer : $("#timer .value"),
+
+	countUp : function() {
+    	counter.val++;
+    	counter.$timer.text(counter.val);
+    	return counter.val;
+    }
+}
+
+var initCounter = window.setInterval(counter.countUp, 100);
+
+
+app = {
+	$body : $("body"),
+
+	vars : {
+		
+		winWidth : ($(document).width())/255,
+		winHeight: ($(document).height())/255,
+		difficulty: 5,
+		previousScore : null,
+		bestScore : null
+	},
+	init : function() {
+		unifiedRandom = randomColorValue();
+		app.$box = $('<div id="box"></div>');
+		app.$box.css("background-color", "rgb("+ randomColorValue() + "," + randomColorValue() + "," + unifiedRandom + ")");
+		app.$box.appendTo("body");
+	},
+
+	reset : function() {
+		unifiedRandom = randomColorValue();
+	    app.$box.css("background-color", "rgb(" + randomColorValue() + "," + randomColorValue() + "," + unifiedRandom + ")");
+	    counter.val = 0;
+	},
+
+	win : function() {
+		var message;
+		var seconds = counter.val / 10;
+
+		if (app.vars.previousScore != null) {
+
+			if (seconds > app.vars.previousScore) {
+				message = "You did it! " + seconds + " seconds isn't bad, but you've done better!";	
+			} else {
+				message = "You did it! And it only took you "+ seconds + " seconds to do it. Now try and beat it!";
+			}
+			
+		} else {
+			message = "You did it! And it only took you " + seconds + " seconds to do it. Now try and beat it!";
+		}
+		
+		alert(message);
+		app.vars.previousScore = seconds;
+		app.reset();
+	},
+
+	updateBackgroundColor : function(e) {
+
+		var $pageX = parseInt(e.pageX / app.vars.winWidth, 10);
+    	var $pageY = parseInt(e.pageY / app.vars.winHeight, 10);
+
+		$body.css("background-color", "rgb(" + $pageX + "," + $pageY + "," + unifiedRandom + ")");
+
+		app.checkForWin();
+	},
+
+	compareColors : function(a, b) {		
+
+		var rDiff = Math.abs(a[0] - b[0]);
+		var gDiff = Math.abs(a[1] - b[1]);
+		var bDiff = Math.abs(a[2] - b[2]);
+
+		if (rDiff < app.vars.difficulty && gDiff < app.vars.difficulty && bDiff < app.vars.difficulty) {			
+			return true;	
+		}
+	},
+
+	checkForWin : function() {
+		var backgroundColor = getTheRGB(app.$body.css("background-color"));
+		var boxColor = getTheRGB(app.$box.css("background-color"));
+
+		if (app.compareColors(backgroundColor, boxColor)) {
+			app.win()
+		}
+	}
+}
+
+app.init();
+
+
+
+
+$(document).mousemove(function(e){
+	
+	
+	app.updateBackgroundColor(e);
+    
+
+}); 
+
+
+
